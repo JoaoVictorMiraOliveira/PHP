678ce0388dbac487593092d149963256f2d3c7d3
Joao Victor Mira de Oliveria <54165704+JoaoVictorMiraOliveira@users.noreply.github.com>
2019-08-22 02:08:30 -0300

    Add files via upload

---
+++ Intro_Aula01.php
@@ -0,0 +1,28 @@
+<?php
+	
+	// Aula 01 - 15.08.2019
+	
+	/* 
+		Segundo Semestre 2019 - Ads
+		Programação Web - Majer
+	*/
+	
+	
+	// (Sinal de Dolar "$" cria uma variavel)
+	
+	$valor1=57;
+	$valor2=3;
+	$total=$valor1+$valor2;
+	echo $total;
+	echo "<br>"; //Quebra de Linhas no codigo Ex: echo "<br>";
+
+	$salario=12590.57;
+	$nome="Joao"; //String - Letras Sempre Entre Aspas Ex: "Joao"
+	$instituicao="Unicid";
+	$solteiro=false;
+	$conveniado=true;
+
+	echo $nome." trabalha na ".$instituicao." e recebe ".$salario //Operador de Concatenação "."(ponto) JUNTA COISAS
+
+
+?>
